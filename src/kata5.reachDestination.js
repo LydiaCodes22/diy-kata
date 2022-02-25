facec2279c86a667f905ea0ade18c99bb8a1dde6
Lydia Wallace <lydia.wallace@outlook.com>
2022-02-25 15:30:28 +0000

    reachDestinations kata complete

--- src/kata5.reachDestination.js
+++ src/kata5.reachDestination.js
@@ -1,3 +1,11 @@
-const reachDestination = (distance, speed) => {};
+/* eslint-disable prettier/prettier */
+const reachDestination = (distance, speed) => {
+    function roundHalf(num) {
+        return Math.round(num*2)/2;
+    }
+    const approxEta = (distance/speed);
+    return `I should be there in ${roundHalf(approxEta)} hours.`
+    
+};
 
 module.exports = reachDestination;
--- test/kata5.reachDestination.test.js
+++ test/kata5.reachDestination.test.js
@@ -1,5 +1,10 @@
+/* eslint-disable prettier/prettier */
 const { reachDestination } = require("../src");
 
 describe("reachDestination", () => {
-  test("returns string with estimated time of arrival", () => {});
+  test("returns string with estimated time of arrival", () => {
+    expect (reachDestination(15,30)).toEqual('I should be there in 0.5 hours.');
+    expect (reachDestination(40,60)).toEqual('I should be there in 0.5 hours.');
+    expect (reachDestination(130,70)).toEqual('I should be there in 2 hours.');
+  });
 });
