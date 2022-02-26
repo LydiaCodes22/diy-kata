75947a52437c4e19cef0e74b48237fd82fe28d1f
Lydia Wallace <lydia.wallace@outlook.com>
2022-02-26 14:06:25 +0000

    getEmployerRole completed including alternative if employee does not exist

--- src/kata7.getEmployerRole.js
+++ src/kata7.getEmployerRole.js
@@ -1,3 +1,10 @@
-const getEmployerRole = (employeeName, employees) => {};
+const getEmployerRole = (employeeName, employees) => {
+  const search = employees.find(x => x.name === employeeName);
+  if (search === undefined) {
+    return "Not an employee";
+  } else {
+    return search.role;
+  }
+};
 
 module.exports = getEmployerRole;
