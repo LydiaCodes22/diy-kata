0373f8151279013adac6e0ac9e977e1f56ca5f5d
Lydia Wallace <lydia.wallace@outlook.com>
2022-02-26 13:13:25 +0000

    joinNames completed

--- src/kata6.joinNames.js
+++ src/kata6.joinNames.js
@@ -1,3 +1,12 @@
-const joinNames = namesObj => {};
+/* eslint-disable prettier/prettier */
+const joinNames = namesObj => {
+   const newArray = namesObj.map(x=> x.name);
+   const newString = `${newArray[newArray.length-2]} & ${newArray[newArray.length-1]}`
+    newArray.splice((newArray.length-2))
+    newArray.push(newString)
+    return newArray.join(', ')
+};
+
+
 
 module.exports = joinNames;
--- test/kata6.joinNames.test.js
+++ test/kata6.joinNames.test.js
@@ -1,5 +1,10 @@
 const { joinNames } = require("../src");
 
 describe("joinNames", () => {
-  test("returns string of names, seperated by commas and an ampersand", () => {});
+  test("returns string of names, seperated by commas and an ampersand", () => {
+    const array1 = [{name: 'Bart'}, {name: 'Lisa'}, {name: 'Maggie'}]
+    const array2 = [{name: 'Steve'}, {name: 'Mike'}, {name: 'Tilly'}, {name: 'Milly'}, {name: 'Rebecca'}]
+    expect(joinNames(array1)).toEqual('Bart, Lisa & Maggie');
+    expect(joinNames(array2)).toEqual('Steve, Mike, Tilly, Milly & Rebecca')
+  });
 });
