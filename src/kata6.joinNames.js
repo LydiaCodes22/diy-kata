7e72869b06ae8c16df87aacd565e862d2855d7dc
Lydia Wallace <lydia.wallace@outlook.com>
2022-02-26 13:33:04 +0000

    alterations to ensure it works with shorter arrays

--- src/kata6.joinNames.js
+++ src/kata6.joinNames.js
@@ -1,10 +1,12 @@
 /* eslint-disable prettier/prettier */
 const joinNames = namesObj => {
-   const newArray = namesObj.map(x=> x.name);
-   const newString = `${newArray[newArray.length-2]} & ${newArray[newArray.length-1]}`
-    newArray.splice((newArray.length-2))
-    newArray.push(newString)
-    return newArray.join(', ')
+   if (namesObj.length >= 2) {
+    const newArray = namesObj.map(x=> x.name);
+    const newString = `${newArray[newArray.length-2]} & ${newArray[newArray.length-1]}`;
+       newArray.splice(newArray.length-2);
+       newArray.push(newString);
+       return newArray.join(', ');
+    } else {return namesObj.join(', ')}
 };
 
 
--- test/kata6.joinNames.test.js
+++ test/kata6.joinNames.test.js
@@ -4,7 +4,9 @@ describe("joinNames", () => {
   test("returns string of names, seperated by commas and an ampersand", () => {
     const array1 = [{name: 'Bart'}, {name: 'Lisa'}, {name: 'Maggie'}]
     const array2 = [{name: 'Steve'}, {name: 'Mike'}, {name: 'Tilly'}, {name: 'Milly'}, {name: 'Rebecca'}]
+    const array3 = [{name: 'Lisa'}, {name: 'Maggie'}]
     expect(joinNames(array1)).toEqual('Bart, Lisa & Maggie');
-    expect(joinNames(array2)).toEqual('Steve, Mike, Tilly, Milly & Rebecca')
+    expect(joinNames(array2)).toEqual('Steve, Mike, Tilly, Milly & Rebecca');
+    expect(joinNames(array3)).toEqual('Lisa & Maggie');
   });
 });
